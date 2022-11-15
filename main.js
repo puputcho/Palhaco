90dfcd7eee201ef4b6b4e6e1091324c2eb9b298b
puputcho <67434177+puputcho@users.noreply.github.com>
2022-11-15 19:56:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() {
+    
+}
+function setup() {
+    canvas = createCanvas(640,480);
+    canvas.position(110, 200);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    image(video, 0,0, 640,480);
+    fill("red")
+    circle(320, 240, 50)
+    fill('black')
+    rect(270, 0, 100, 100);
+    rect(220, 100, 200, 50);
+    
+}
+function tirarFoto() {
+    save('aloha.png');
+}
